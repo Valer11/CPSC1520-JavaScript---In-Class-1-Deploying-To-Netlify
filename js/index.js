959db58d39052dbd2ebd4cf8692d0cbfdced36b1
Valer11 <Valer11@users.noreply.github.com>
2021-09-10 18:54:49 -0600

    In class 1 done
    First in class assignment is done

--- js/index.js
+++ js/index.js
@@ -1,2 +1,21 @@
-var message = document.querySelector("main>section>h2").innerHTML;
-console.log(message);
+var h1 = document.querySelector("h1").innerHTML;
+console.log(h1);
+
+var pic = document.querySelector("main>section>figure>img").innerHTML;
+console.log(pic);
+
+var paragraph = document.querySelector("main>section>p").innerHTML;
+console.log(paragraph);
+
+var h2 = document.querySelector("h2.intro").innerHTML;
+console.log(h2);
+
+var firstName;
+firstName = 'Valerii Dombrovskyi';
+console.log(firstName);
+
+var intro;
+intro = document.querySelector("main>section>h2").innerHTML;
+console.log(intro);
+intro = 'Chaning intro into something new';
+console.log(intro);
